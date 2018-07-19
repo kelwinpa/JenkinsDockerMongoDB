b52a295210b4bbe4f813e6c6c9ba2229c2ef6bdc
Kelwin Payares <Kelwin Payares@250-1-58.ELIS.ORG>
2018-07-19 17:18:47 +0200

    -first commit

---
+++ src/main/java/com/jenkinsDockerMongoDB/jenkinsDockerMongoDB/JenkinsDockerMongoDbApplication.java
@@ -0,0 +1,12 @@
+package com.jenkinsDockerMongoDB.jenkinsDockerMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerMongoDbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jenkinsDockerMongoDB/jenkinsDockerMongoDB/JenkinsDockerMongoDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jenkinsDockerMongoDB.jenkinsDockerMongoDB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsDockerMongoDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
